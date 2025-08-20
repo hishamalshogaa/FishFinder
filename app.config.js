bec73fb0733a988f4ba8264d03bcaddab80f72c7
hishamalshogaa <hishamalshogaa@gmail.com>
2025-08-21 01:43:35 +0300

    Create app.config.js

---
+++ app.config.js
@@ -0,0 +1,22 @@
+{
+  "expo": {
+    "name": "RedSeaFishFinder",
+    "slug": "redsea-fish-finder",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "splash": {
+      "image": "./assets/splash.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "updates": {
+      "fallbackToCacheTimeout": 0
+    },
+    "assetBundlePatterns": ["**/*"],
+    "android": {
+      "package": "com.redsea.fishfinder",
+      "versionCode": 1
+    }
+  }
+}
